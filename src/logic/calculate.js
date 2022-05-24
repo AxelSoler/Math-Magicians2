67ba9f98eb6cb16154faece50037a93005809f02
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-05-24 14:33:56 -0300

    operate

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,1 +1,1 @@
-import Calculator from './components/Calculator';
+import operate from './operate';
--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,1 +1,30 @@
-import Calculator from './components/Calculator';
+import Big from 'big.js';
+
+export default function operate(number1, number2, calc) {
+  const obj1 = Big(number1);
+  const obj2 = Big(number2);
+
+  if (calc === '%') {
+    try {
+      return obj1.mod(obj2).toString();
+    } catch (err) {
+      return "Error";
+    }
+  }
+  if (calc === '&divide;') {
+    try {
+      return obj1.div(obj2).toString();
+    } catch (err) {
+      return "Error dividing by 0.";
+    }
+  }
+  if (calc === 'x') {
+    return obj1.times(obj2);
+  }
+  if (calc === '-') {
+    return obj1.minus(obj2);
+  }
+  if (calc === '+') {
+    return obj1.plus(obj2);
+  } 
+}
