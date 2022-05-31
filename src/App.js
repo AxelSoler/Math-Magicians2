510660fcaf7aa65f3cf1b5fd91acb67058356509
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-05-31 14:10:37 -0300

    route for calculator

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import Calculator from './components/Calculator';
 import Home from './components/Home';
@@ -7,8 +7,15 @@ import Home from './components/Home';
 const App = () => (
   <div className="App">
     <h1>Math Magicians</h1>
+    <nav>
+      <ul>
+        <Link to="/">Home</Link>
+        <Link to="Calculator">Calculator</Link>
+        {/* <Link to="Home">Home</Link> */}
+      </ul>
+    </nav>
     <Routes>
-      <Route path="Home" element={<Home />} />
+      <Route path="/" element={<Home />} />
       <Route path="Calculator" element={<Calculator />} />
       {/* <Route path="Quote" element={<Quote />} /> */}
     </Routes>
