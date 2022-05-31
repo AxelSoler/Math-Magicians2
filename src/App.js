e2c981500860cdcfb4f78bc1ed51eb969488e28b
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-05-31 14:05:11 -0300

    create home page and started working with routers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Calculator from './components/Calculator';
+import Home from './components/Home';
 
 const App = () => (
   <div className="App">
-    <Calculator />
+    <h1>Math Magicians</h1>
+    <Routes>
+      <Route path="Home" element={<Home />} />
+      <Route path="Calculator" element={<Calculator />} />
+      {/* <Route path="Quote" element={<Quote />} /> */}
+    </Routes>
   </div>
 );
 
---
+++ src/components/Home.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Home = () => {
+  <div>
+    <h2>Welcome to our page!</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Maecenas semper sem nibh, quis posuere purus egestas quis.
+      Nullam mattis sit amet metus a rhoncus.
+      Mauris at ex eu mauris mattis tempor nec vel eros.
+      Integer sit amet tempus libero.
+      Suspendisse risus orci, lobortis a tempus at, semper vel nisl.
+      Quisque faucibus lacus eget elit interdum, ac feugiat ex tincidunt.
+      Phasellus ultricies eros tincidunt dignissim lacinia.
+      Etiam posuere commodo elit eget euismod.
+    </p>
+    <p>
+      Mauris molestie gravida iaculis.
+      Vestibulum semper velit sit amet bibendum porta.
+      Proin efficitur justo quam, vitae sodales sem placerat ut.
+      Phasellus blandit sem eu lorem dignissim, ut lacinia lectus viverra.
+      Ut nibh nulla, blandit imperdiet neque tristique, faucibus tempus risus.
+      Etiam vestibulum ipsum nec enim malesuada, quis varius metus consequat.
+      Aenean fermentum cursus aliquet.
+      Fusce neque sapien, tristique ac eros quis, tristique finibus lectus.
+      Fusce mattis felis ut libero laoreet interdum.
+    </p>
+  </div>;
+};
+
+export default Home;
